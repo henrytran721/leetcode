aece8468b711dc10f91e6777c5cdcb1662f26276
henrytran721 <henryhtran07@gmail.com>
2021-01-19 13:04:59 -0800

    singly linked list update

---
+++ data_structures/singly_linked_list.js
@@ -0,0 +1,169 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    // remove from end of linked list
+    pop() {
+        if(!this.length) return undefined;
+        var current = this.head;
+        var newTail = current;
+        // traverse to the tail
+        // break out of loop when current is element before null
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        // set up pointers for new tail
+        this.tail = newTail;
+        this.tail.next = null;
+        // decrement length
+        this.length--;
+        // if last node in linkedlist, set both head and tail to null
+        if(!this.length) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+
+    // remove from beginning of linked list
+    shift() {
+        if(!this.length) return undefined;
+        var oldHead = this.head;
+        this.head = oldHead.next;
+        this.length--;
+        oldHead.next = null;
+        if(!this.length) {
+            this.head = null;
+            this.tail = null;
+        }
+        return oldHead;
+    }
+
+    // insert at the beginning
+    unshift(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(i) {
+        var counter = 0;
+        if(i < 0 || i > this.length) return null;
+        var current = this.head;
+        while(counter !== i) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+
+    set(i, val) {
+        var foundNode = this.get(i);
+        if(foundNode) {
+            foundNode.val = val;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    insert(i, val) {
+        var newNode = new Node(val);
+        if(i < 0 || i > this.length) return false;
+        if(i === 0) {
+            return this.unshift(val);
+        } else if(i === this.length) {
+            return this.push(val);
+        } else {
+            var prevNode = this.get(i - 1);
+            var nextNode = prevNode.next;
+            prevNode.next = newNode;
+            newNode.next = nextNode;
+            this.length++;
+            return this;
+        }
+    }
+
+    remove(i) {
+        if(i < 0 || i > this.length - 1) return false;
+        if(i === 0) {
+            return this.shift();
+        } else if(i === this.length - 1) {
+            return this.pop();
+        } else {
+            var prevNode = this.get(i - 1);
+            var deletedNode = prevNode.next;
+            var nextNode = deletedNode.next;
+            prevNode.next = nextNode;
+            deletedNode.next = null;
+            this.length--;
+            return deletedNode;
+        }
+    }
+
+    // print all nodes by inserting into array
+    print() {
+        var res = [];
+        var current = this.head;
+        while(current) {
+            res.push(current.val);
+            current = current.next;
+        }
+        return res;
+    }
+
+    reverse() {
+        var prev = null;
+        var next = null;
+        var node = this.head;
+        
+        this.head = this.tail;
+        this.tail = node;
+        for(let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+    }
+}
+
+
+var sll = new SinglyLinkedList();
+sll.push(1);
+sll.push(2);
+sll.push(3);
+sll.unshift(0);
+sll.reverse();
+console.log(sll.print());
