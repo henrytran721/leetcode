988a56f575280a362bbfa0f969f1c89224ac4d8a
henrytran721 <henryhtran07@gmail.com>
2021-03-15 11:42:22 -0700

    reverseWords

--- algorithms/productExceptSelf.js
+++ algorithms/productExceptSelf.js
@@ -47,3 +47,31 @@ var productExceptSelf = function(nums) {
         
         return result;
     };
+
+
+    // left and right approach
+    // Time: O(n)
+    // Space: O(n)
+
+    var productExceptSelf = function(nums) {
+        let left = [];
+        let right = [];
+        let result = [];
+        
+        // left: [1, 2, 6, 24]
+        left[0] = 1;
+        for(let i = 1; i < nums.length; i++) {
+            left[i] = nums[i - 1] * left[i - 1];
+        }
+        
+        right[nums.length - 1] = 1;
+        for(let i = nums.length - 2; i >= 0; i--) {
+            right[i] = nums[i + 1] * right[i + 1];
+        }
+        
+        for(let i = 0; i < nums.length; i++) {
+            result[i] = left[i] * right[i];
+        }
+        
+        return result;
+    };
