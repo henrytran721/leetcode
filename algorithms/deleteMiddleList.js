1f238bf11efef234f7edd59589e4bd5597dfc48f
henrytran721 <henryhtran07@gmail.com>
2021-03-23 11:30:11 -0700

    edgecase to dml

--- algorithms/deleteMiddleList.js
+++ algorithms/deleteMiddleList.js
@@ -44,8 +44,10 @@ class Node {
     }
   
     deleteMiddle(node) {
+      if(node == null || node.next == null) return false;
       node.val = node.next.val;
       node.next = node.next.next;
+      return true;
     }
   
   }
