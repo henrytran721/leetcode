63d894c54b1811407280c3e7d18a94216b76f514
henrytran721 <henryhtran07@gmail.com>
2021-03-23 11:26:15 -0700

    delete node in middle of ll

---
+++ algorithms/deleteMiddleList.js
@@ -0,0 +1,62 @@
+// Delete the middle node (without given access to the head or tail) from the singly linked list
+// a -> b -> c -> d -> e -> f
+// a -> b -> d -> e -> f
+
+
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+  
+  class SinglyLinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+  
+    print() {
+      let result = [];
+      let current = this.head;
+  
+      while(current !== null) {
+        result.push(current.val);
+        current = current.next;
+      }
+  
+      return result;
+    }
+  
+    push(val) {
+      var newNode = new Node(val);
+      if(!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+  
+      this.length++;
+      return this;
+    }
+  
+    deleteMiddle(node) {
+      node.val = node.next.val;
+      node.next = node.next.next;
+    }
+  
+  }
+  
+  let sll = new SinglyLinkedList();
+  sll.push('a');
+  sll.push('b');
+  sll.push('c');
+  sll.push('d');
+  sll.push('e');
+  sll.push('f');
+  console.log(sll.print());
+  console.log(sll.deleteMiddle(sll.head.next));
+  console.log(sll.print());
